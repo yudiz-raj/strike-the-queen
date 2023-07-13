3f0480ef104e4cbde1eb52d434b17689060285c9
yudiz-raj <raj.patel@yudiz.com>
2023-07-13 14:21:13 +0530

    support for mobile

--- src/main.js
+++ src/main.js
@@ -7,58 +7,30 @@ const init = () => {
 			);
 		}
 	}
-	if (window.innerWidth < 1050) {
-		var game = new Phaser.Game({
-			width: 720,
-			height: 1180,
-			type: Phaser.AUTO,
-			backgroundColor: "#242424",
-			parent: "game-division",
-			scale: {
-				mode: Phaser.Scale.FIT,
-				autoCenter: Phaser.Scale.CENTER_BOTH,
-				orientation: Phaser.Scale.Orientation.PORTRAIT,
-			},
-			audio: {
-				disableWebAudio: false,
-			},
-			dom: {
-				createContainer: true,
-			},
-			physics: {
-				default: "arcade",
-				arcade: {
-					gravity: { y: 0 },
-					debug: false
-				}
+	var game = new Phaser.Game({
+		width: 1920,
+		height: 1080,
+		type: Phaser.AUTO,
+		backgroundColor: "#242424",
+		parent: "game-division",
+		scale: {
+			mode: Phaser.Scale.FIT,
+			autoCenter: Phaser.Scale.CENTER_BOTH,
+		},
+		audio: {
+			disableWebAudio: false,
+		},
+		dom: {
+			createContainer: true,
+		},
+		physics: {
+			default: "arcade",
+			arcade: {
+				gravity: { y: 0 },
+				debug: false
 			}
-		});
-	} else {
-		var game = new Phaser.Game({
-			width: 1920,
-			height: 1080,
-			type: Phaser.AUTO,
-			backgroundColor: "#242424",
-			parent: "game-division",
-			scale: {
-				mode: Phaser.Scale.FIT,
-				autoCenter: Phaser.Scale.CENTER_BOTH,
-			},
-			audio: {
-				disableWebAudio: false,
-			},
-			dom: {
-				createContainer: true,
-			},
-			physics: {
-				default: "arcade",
-				arcade: {
-					gravity: { y: 0 },
-					debug: false
-				}
-			}
-		});
-	}
+		}
+	});
 	game.scene.add("Preload", Preload);
 	game.scene.add("Boot", Boot, true);
 };
